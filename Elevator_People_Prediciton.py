ea952b860989b8207d1e2e8652e5b9936ae3d9ae
Kevin Wijaya <81699770+xxKeyaxx@users.noreply.github.com>
2023-12-06 21:12:24 +0700

    Update Elevator_People_Prediciton.py

--- Elevator_People_Prediciton.py
+++ Elevator_People_Prediciton.py
@@ -5,7 +5,6 @@ import numpy as np
 from ultralytics import YOLO
 import supervision as sv
 from PIL import Image
-# streamlit run Elevator_People_Prediciton.py
 
 def process_image(uploaded_file):
     # Convert the uploaded image to a cv2-style image
@@ -18,11 +17,12 @@ def convert_to_streamlit_format(cv2_image):
     rgb_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
     return rgb_image
 
-# print(dir(sv))
 st.title("Elevator People Prediction")
 model = YOLO('yolov8m.pt')
+detection1 = 0
+detection2 = 0
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload the image for the inside of the elevator ...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = process_image(uploaded_file)    
@@ -30,11 +30,37 @@ if uploaded_file is not None:
     
     result = list(result)[0]
     detections = sv.Detections.from_yolov8(result)
-    detections = detections[(detections.class_id == 0) & (detections.confidence > 0.2)]
-    
+    detections = detections[(detections.class_id == 0) & (detections.confidence > 0.3)]
     box_annotator = sv.BoxAnnotator()
     image = box_annotator.annotate(scene=image, detections=detections)
     image = convert_to_streamlit_format(image)
     st.write("Output Image:")
     st.image(image)
-    st.write("There are", len(detections), "people in the elevator")
+    detection1 = len(detections)
+    st.write("There are", detection1, "people in the elevator")
+
+uploaded_file = st.file_uploader("Upload an image for the people waiting...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = process_image(uploaded_file)    
+    result = model(image)
+    
+    result = list(result)[0]
+    detections = sv.Detections.from_yolov8(result)
+    detections = detections[(detections.class_id == 0) & (detections.confidence > 0.3)]
+    box_annotator = sv.BoxAnnotator()
+    image = box_annotator.annotate(scene=image, detections=detections)
+    image = convert_to_streamlit_format(image)
+    st.write("Output Image:")
+    st.image(image)
+    detection2 = len(detections)
+    st.write("There are", detection2, "people waiting")
+
+maxPeople = st.number_input("Enter elevator maximum people carrying capacity", step=1, value=0, format="%d")
+
+if(detection1 < maxPeople and detection2 > 0):
+    st.write("Elevator make a stop")
+elif(detection1 == 0 and detection2 == 0):
+    st.write("")
+else:
+    st.write("Elevator keep going")
